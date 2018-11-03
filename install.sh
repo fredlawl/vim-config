70d8cd2902e309eb6e264a8b4c599c5d1d6c2a63
Frederick Lawler <fred@fredlawl.com>
2018-11-03 15:48:37 -0500

    Add install script for easy installation

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+vimrc=$HOME/.vimrc
+vim=$HOME/.vim
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "Going to install vim-config to " $HOME
+read -p "Are you sure? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+	echo "Exiting..."
+	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+if [[ -L "$vimrc" ]]
+then
+	echo "Unlinking $vimrc"
+	rm "$vimrc"
+elif [[ -f $vimrc ]]
+then
+	echo "Backing up $vimrc"
+	mv "$vimrc" "$vimrc.bak"
+fi
+
+if [[ -L $vim ]]
+then
+	echo "Unlinking $vim"
+	rm "$vim"
+elif [[ -f $vim ]]
+then
+	echo "Backing up $vim"
+	mv "$vim" "$vim.bak"
+fi
+
+echo "Linking configuration"
+$(ln -s "$dir/.vimrc" "$vimrc")
+$(ln -s "$dir/.vim" "$vim")
